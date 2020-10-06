54b58c3100eaa3701bcc17041974df0c6c83a870
Ikhsan sugiarto <48153104+sanengineer@users.noreply.github.com>
2020-10-06 11:25:31 +0700

    update text

--- Models/user/user-game-biodata.js
+++ Models/user/user-game-biodata.js
@@ -1,20 +1,18 @@
-const { Sequelize } = require("sequelize/types");
+module.exports = (sequelize, Sequelize) => {
+  const usrGameBio = sequelize.define("user_game_biodata", {
+    user_id: {
+      type: Sequelize.STRING,
+    },
+    fullname: {
+      type: Sequelize.STRING,
+    },
+    sex: {
+      type: Sequelize.STRING,
+    },
+    jobs: {
+      type: Sequelize.STRING,
+    },
+  });
 
-module.exports =(sequelize, Sequelize) => {
-    const usrGameBio = sequelize.define("user_game_biodata", {
-        user_id: {
-            type: Sequelize.STRING,
-        },
-        fullname: {
-            type: Sequelize.STRING,
-        },
-        sex: {
-            type: Sequelize.STRING,
-        },
-        jobs: {
-            type: Sequelize.STRING,
-        }
-    })
-
-    return usrGameBio
-}
+  return usrGameBio;
+};
