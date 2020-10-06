48e393127bab101ffdc7907c44e6943ddb933533
Ikhsan sugiarto <48153104+sanengineer@users.noreply.github.com>
2020-10-06 23:51:09 +0700

    update crud code

--- Controllers/controller-api-user-game.js
+++ Controllers/controller-api-user-game.js
@@ -27,3 +27,59 @@ exports.create = (req, res) => {
       });
     });
 };
+
+// get one user by id
+exports.getOne = (req, res) => {
+  const id = req.params.id;
+
+  UserGame.findByPk(id)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "no user with" + id,
+      });
+    });
+};
+
+//delete user by id
+exports.deleteOne = (req, res) => {
+  const id = req.params.id;
+
+  UserGame.destroy({ where: { id: id } })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "user by id was deleted successfully",
+        });
+      } else {
+        res.send({
+          message: `can't delete user with id=${id}`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "can't delete user with id=" + id,
+      });
+    });
+};
+
+//delete all user
+exports.deleteAll = (req, res) => {
+  UserGame.destroy({
+    where: {},
+    truncate: false, // apa itu truncate??
+  })
+    .then((nums) => {
+      res.send({
+        message: `${nums} Users was delete successfully`,
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Delete all failed",
+      });
+    });
+};
