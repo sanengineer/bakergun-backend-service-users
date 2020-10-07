f13006b3496ae90f6217040d50eb4c1669d40062
sanengineer <48153104+sanengineer@users.noreply.github.com>
2020-10-07 23:58:06 +0700

    update model

--- Models/user/user-game-biodata.js
+++ Models/user/user-game-biodata.js
@@ -1,5 +1,8 @@
 module.exports = (sequelize, Sequelize) => {
   const usrGameBio = sequelize.define("user_game_biodata", {
+    user_game_biodata_id: {
+      type: Sequelize.INTEGER,
+    },
     fullname: {
       type: Sequelize.STRING,
     },
