0e8f278cf55ec56c6d41008ebf8e9f8550e96268
sanengineer <48153104+sanengineer@users.noreply.github.com>
2020-10-29 17:42:16 +0700

    update code

--- Models/user/user-game.js
+++ Models/user/user-game.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs");
+
 module.exports = (sequelize, Sequelize) => {
   const usrGame = sequelize.define("user_game", {
     user_id: {
@@ -23,5 +25,23 @@ module.exports = (sequelize, Sequelize) => {
     },
   });
 
+  usrGame.beforeSave((user, options) => {
+    if (user.changed("password")) {
+      user.password = bcrypt.hashSync(
+        user.password,
+        bcrypt.genSaltSync(31),
+        null
+      );
+    }
+  });
+
+  usrGame.prototype.comparePassword = function (passw, callbck) {
+    bcrypt.compare(passw, this.password, function (err, isMatch) {
+      if (err) {
+        return callbck(err);
+      }
+      callbck(null, isMatch);
+    });
+  };
   return usrGame;
 };
