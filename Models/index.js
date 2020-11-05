63dbeeca3f3e0bf0781748d80ef129700c9e4eec
sanengineer <48153104+sanengineer@users.noreply.github.com>
2020-11-05 09:04:03 +0700

    update config db

--- Models/index.js
+++ Models/index.js
@@ -14,7 +14,7 @@ const Sequelize = require("sequelize");
 // console.log(dbConfig);
 
 const sequelize = new Sequelize(
-  process.env.NAME,
+  process.env.DB,
   process.env.USERNAME,
   process.env.PASSWORD,
   {
